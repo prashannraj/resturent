0d51cde4457d47a7ab194676e72c7c5cd83bd1ce
prashannraj <prashannraj@hotmail.com>
2024-02-02 13:13:39 +0545

    config file created

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">APPAN RESUTRENT</div>
+          <div className="right">
+            <p>JANAKPURDHAM, NEPAL</p>
+            <p>Open: 08:00 AM - 12:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Prashann Raj</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Prashann Raj.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
